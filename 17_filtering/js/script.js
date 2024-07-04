b92f1aa1c686215c202a789a36b6eed19b024e27
Mizushino0904 <157794791+Mizushino0904@users.noreply.github.com>
2024-07-04 15:32:09 +0900

    Add files via upload

---
+++ 17_filtering/js/script.js
@@ -0,0 +1,28 @@
+$(function(){
+    //ボタンを選択
+    $("button").click(function(){
+        //value属性の値を取得
+        var target = $(this).attr("value");
+
+        ////currentクラス
+        //全てのボタンからcurrentクラスを削除
+        $("button").removeClass("current");
+        //クリックされたボタンにcurrentクラスを追加
+        $(this).addClass("current");
+
+        //全てのli要素を調べる
+        $("#list li").each(function(){
+            //一度全て非表示にする
+            $(this).animate({"opacity" : 0 }, 300, function(){
+                $(this).hide();
+
+                //フィルタリングの条件を満たしているか
+                if($(this).hasClass(target) || target == "all"){
+                    //条件を満たしている場合は再表示
+                    $(this).show().animate({ "opacity" : 1 }, 300 );
+                }
+
+            });
+        });
+    });
+});
